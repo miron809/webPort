cf66669c7602ea7747ffb2de6d78842ff67cf9f3
Dmitriy Mironenko <dim@uain.su>
2020-04-14 14:48:00 +0300

    updated project

--- src/assets/scripts/scrollingLettersAnimation.js
+++ src/assets/scripts/scrollingLettersAnimation.js
@@ -1,105 +1,109 @@
-import imagesLoaded from './vendor/imagesloaded.pkgd.min';
 import charming from './vendor/charming.min'
-/**
- * demo1.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2018, Codrops
- * http://www.codrops.com
- */
 
-const title = document.querySelector('.first-title');
+const navigation = document.getElementById('scroll-navigation');
+const navigationHeight = document.querySelector('.scroll-navigation li').offsetHeight;
+const navigationTop = navigation.offsetTop;
 const welcomeSpan = document.querySelector('.welcome');
-const vh = Math.round(Math.max(document.documentElement.clientHeight, window.innerHeight || 0));
+const firstSectionHeight = document.getElementById('homeSection').offsetHeight;
 
-function scrollingTitle() {
-  if (window.scrollY > vh * 0.6) {
-    welcomeSpan.classList.remove('show-welcome');
-    title.classList.remove('top');
-    title.classList.add('bottom');
+function titleScrolling() {
+  let scrollPosition = window.scrollY;
+  //fixing title to header
+  if (scrollPosition > navigationTop - 100) {
+    navigation.classList.add('fixed');
+    welcomeSpan.classList.add('hide-welcome');
   } else {
-    welcomeSpan.classList.add('show-welcome');
-    title.classList.add('top');
-    title.classList.remove('bottom');
+    navigation.classList.remove('fixed');
+    welcomeSpan.classList.remove('hide-welcome');
+  }
+
+  //changing title to left position
+  if (scrollPosition > firstSectionHeight - 100 - navigationHeight) {
+    navigation.classList.add('scrolling');
+  } else {
+    navigation.classList.remove('scrolling')
   }
 }
 
-window.addEventListener('scroll', scrollingTitle);
+const sectionArr = document.querySelectorAll("section");
+const sections = [];
+const titleArr = document.querySelectorAll('.scroll-navigation li h2');
+const chars = ['$', '%', '#', '@', '&', '=', '*', '/'];
+const charsTotal = chars.length;
+let currentSection = document.querySelector(`.scroll-navigation li.active`).getAttribute('data-section');
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-{
-  const chars = ['$','%','#','@','&','=','*','/'];
-  const charsTotal = chars.length;
-  const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+//using charming
+titleArr.forEach(title => {
+  charming(title);
+});
 
-  class Entry {
-    constructor(el) {
-      this.DOM = {el: el};
-      this.DOM.image = this.DOM.el.querySelector('.content__img');
-      this.DOM.title = {word: this.DOM.el.querySelector('.content__text')};
-      charming(this.DOM.title.word);
-      this.DOM.title.letters = Array.from(this.DOM.title.word.querySelectorAll('span'));
-      this.DOM.title.letters.forEach(letter => letter.dataset.initial = letter.innerHTML);
-      this.lettersTotal = this.DOM.title.letters.length;
-      observer.observe(this.DOM.el);
+//creating section arrList
+sectionArr.forEach(item => {
+  sections.push(
+    {
+      'id': item.id,
+      'top': item.offsetTop,
+      'bottom': item.offsetTop + item.offsetHeight
     }
-    enter(direction = 'down') {
-      this.DOM.title.word.style.opacity = 1;
+  )
+});
 
-      this.timeouts = [];
-      this.complete = false;
-      let cnt = 0;
-      this.DOM.title.letters.forEach((letter, pos) => {
-        const timeout = setTimeout(() => {
-          letter.innerHTML = chars[getRandomInt(0,charsTotal-1)];
-          setTimeout(() => {
-            letter.innerHTML = letter.dataset.initial;
-            ++cnt;
-            if ( cnt === this.lettersTotal ) {
-              this.complete = true;
-            }
-          }, 100);
-        }, pos*80);
-        this.timeouts.push(timeout);
-      });
-    }
-    exit(direction = 'down') {
-      this.DOM.title.word.style.opacity = 0;
-      if ( this.complete ) return;
-      for ( let i = 0, len = this.timeouts.length; i <= len - 1; ++i ) {
-        clearTimeout(this.timeouts[i]);
-      }
+//spy for scroll position - detect the active section
+function scrollingSpy() {
+  let scrollPosition = window.scrollY;
+  for (let section of sections) {
+    if (section.top <= scrollPosition && scrollPosition <= section.bottom) {
+      setCurrentSection(section.id);
+      document.querySelector('.scroll-navigation li.active').classList.remove('active');
+      document.querySelector(`[data-section=${section.id}]`).classList.add('active');
     }
   }
+}
 
-  let observer;
-  let current = -1;
-  let allentries = [];
-  const sections = Array.from(document.querySelectorAll('.content__section'));
-  // Preload all the images in the page..
-  imagesLoaded(document.querySelectorAll('.content__img'), () => {
-    document.body.classList.remove('loading');
-    if ('IntersectionObserver' in window) {
-      document.body.classList.add('ioapi');
+//set current section and start letters changing
+function setCurrentSection(newCurrent) {
+  if (newCurrent !== currentSection) {
+    lettersChanging(newCurrent);
+    currentSection = newCurrent;
+  }
+}
 
-      observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if ( entry.intersectionRatio > 0.5 ) {
-            const newcurrent = sections.indexOf(entry.target);
-            if ( newcurrent === current ) return;
-            const direction = newcurrent > current;
-            if (current >= 0 ) {
-              allentries[current].exit(direction ? 'down' : 'up');
-            }
-            allentries[newcurrent].enter(direction ? 'down' : 'up');
-            current = newcurrent;
-          }
-        });
-      }, { threshold: .5 });
+//letters changing
+function lettersChanging(id) {
+  const letters = Array.from(document.querySelectorAll(`[data-section=${id}] span`));
+  const lettersTotal = letters.length;
+  let cnt = 0;
 
-      sections.forEach(section => allentries.push(new Entry(section)));
+  //save letters from html to data-src & data-initial
+  letters.forEach(letter => {
+    if (letter.dataset.src) {
+      letter.dataset.initial = letter.dataset.src
+    } else {
+      letter.dataset.src = letter.innerHTML;
+      letter.dataset.initial = letter.dataset.src
     }
   });
+
+  //change letters
+  letters.forEach((letter, pos) => {
+    setTimeout(() => {
+      letter.innerHTML = chars[getRandomInt(0, charsTotal - 1)];
+      setTimeout(() => {
+        letter.innerHTML = letter.dataset.initial;
+        ++cnt;
+        if (cnt === lettersTotal) {}
+      }, 100);
+    }, pos * 80);
+  });
 }
+
+
+function scrollInit() {
+  titleScrolling();
+  scrollingSpy();
+}
+
+lettersChanging(currentSection);
+
+window.addEventListener('scroll', scrollInit);
